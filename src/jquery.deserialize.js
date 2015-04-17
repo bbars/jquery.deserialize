442f620a570f7d89c47daa02c8e1cb391c85da06
Denis Borzenko <b-bars@mail.ru>
2015-04-17 20:52:43 +0300

    Update jquery.deserialize.js

--- src/jquery.deserialize.js
+++ src/jquery.deserialize.js
@@ -1,104 +1,169 @@
 (function($)
-	{
-	/**
-	 * jQuery.deserialize plugin
-	 * Fills elements in selected containers with data extracted from URLencoded string
-	 * @param data URLencoded data or array of key-value pairs (jQuery.serializeArray() compatible) or plain object
-	 * @param clearForm if true form will be cleared prior to deserialization
-	 */
-	$.fn.deserialize = function(data, clearForm)
-		{
-		this.each(function()
-			{
-			deserialize(this, data, !!clearForm);
-			});
-		};
+    {
+    /**
+     * jQuery.deserialize plugin
+     * Fills elements in selected containers with data extracted from URLencoded string
+     * @param data URLencoded data or array of key-value pairs (jQuery.serializeArray() compatible) or plain object
+     * @param cfg || cfg.clearForm if true form will be cleared prior to deserialization
+     */
+    $.fn.deserialize = function(data, cfg)
+        {
+        this.each(function()
+            {
+            deserialize(this, data, cfg);
+            });
+        return this;
+        };
 
-	/**
-	 * Fills specified form with data extracted from string
-	 * @param element form to fill
-	 * @param data URLencoded data or array of key-value pairs (jQuery.serializeArray() compatible) or plain object
-	 * @param clearForm if true form will be cleared prior to deserialization
-	 */
-	function deserialize(element, data, clearForm)
-		{
-		var keyvals = {},
-			i = 0,
-			split = null,
-			key = null,
-			value = null,
-			splitParts = null,
-			selectorElemsVal = 'select,input:not([type=submit],[type=button],[type=reset],[type=reset],[type=checkbox],[type=radio]),textarea',
-			selectorElemsChecked = 'input[type="checkbox"],input[type="radio"]';
-		
-		if (clearForm)
-			{
-			$(selectorElemsChecked, element).removeAttr('checked');
-			$(selectorElemsVal, element).val('');
-			}
-		
-		if (typeof data == 'string')
-			{
-			data = data.split('&');
-			while (split = data[i++])
-				{
-				splitParts = split.split('=', 2);
-				key = decodeURIComponentPlus(splitParts[0] || '');
-				value = splitParts.length > 1 ? decodeURIComponentPlus(splitParts[1]) : true;
-				
-				if (key != '')
-					{
-					if (key in keyvals)
-						{
-						if ($.type(keyvals[key]) !== 'array')
-							keyvals[key] = [keyvals[key]];
-						
-						keyvals[key].push(value);
-						}
-					else
-						keyvals[key] = value;
-					}
-				}
-			}
-		else if (typeof data == 'object')
-			{
-			if (data.constructor.name == 'Array')
-				{
-				for (i = 0; i < data.length; i++)
-					keyvals[data[i].name] = data[i].value;
-				}
-			else
-				keyvals = data;
-			}
-		
-		for (key in keyvals)
-			{
-			value = keyvals[key];
-			
-			/*
-			// Guess checkboxes mode:
-			$(selectorElemsChecked, element)
-				.filter(function()
-					{
-					return this.name == key && ($(this).val() == value || $(this).is('[type=checkbox]'));
-					})
-				.prop('checked', !!value);
-			*/
-			
-			$(selectorElemsChecked, element)
-				.filter('[name="'+ key +'"]')
-				.prop('checked', false)
-				.filter(function() { return $(this).val() == value; })
-				.prop('checked', true);
-			$(selectorElemsVal, element)
-				.filter('[name="'+ key +'"]')
-				.val(value);
-			}
-		}
-		
-	function decodeURIComponentPlus(s)
-		{
-		return decodeURIComponent(s ? (s+'').replace(/\+/g, '%20') : '');
-		}
-	
-	})(jQuery);
+    /**
+     * Fills specified form with data extracted from string
+     * @param element form to fill
+     * @param data URLencoded data or array of key-value pairs (jQuery.serializeArray() compatible) or plain object
+     * @param cfg || cfg.clearForm if true form will be cleared prior to deserialization
+     */
+    function deserialize(element, data, _cfg)
+        {
+        var $element = $(element),
+            keyvals = {},
+            i = 0,
+            split = null,
+            key = null,
+            value = null,
+            splitParts = null,
+            selectorElemsVal = 'select,input:not([type=submit],[type=button],[type=reset],[type=reset],[type=checkbox],[type=radio]),textarea',
+            selectorElemsChecked = 'input[type="checkbox"],input[type="radio"]';
+        
+        var cfg = {
+            clearForm: false,
+            triggerChange: [],
+            onUnknown: null, // function() {...} might be here
+            };
+        
+        if (typeof _cfg == 'object')
+            {
+            for (var k in _cfg)
+                cfg[k] = _cfg[k];
+            }
+        else
+            cfg.clearForm = !!_cfg;
+        
+        if (cfg.clearForm)
+            {
+            $element.find(selectorElemsChecked).removeAttr('checked');
+            $element.find(selectorElemsVal).val('');
+            }
+        
+        if (typeof data == 'string')
+            {
+            data = data.split('&');
+            while (split = data[i++])
+                {
+                splitParts = split.split('=', 2);
+                key = decodeURIComponentPlus(splitParts[0] || '');
+                value = splitParts.length > 1 ? decodeURIComponentPlus(splitParts[1]) : true;
+                
+                if (key != '')
+                    {
+                    if (key in keyvals)
+                        {
+                        if ($.type(keyvals[key]) !== 'array')
+                            keyvals[key] = [keyvals[key]];
+                        
+                        keyvals[key].push(value);
+                        }
+                    else
+                        keyvals[key] = value;
+                    }
+                }
+            }
+        else if (typeof data == 'object')
+            {
+            if (data.constructor.name == 'Array')
+                {
+                for (i = 0; i < data.length; i++)
+                    keyvals[data[i].name] = data[i].value;
+                }
+            else
+                keyvals = data;
+            keyvals = toOneLevelObject(keyvals);
+            }
+        
+        if (!cfg.triggerChange)
+            cfg.triggerChange = false;
+        else if (typeof cfg.triggerChange != 'object' || cfg.triggerChange.constructor.name != 'Array')
+            {
+            cfg.triggerChange = [];
+            for (key in keyvals)
+                cfg.triggerChange.push(key);
+            }
+        
+        for (key in keyvals)
+            {
+            value = keyvals[key];
+            
+            /*
+            // Guess checkboxes mode:
+            $(selectorElemsChecked, element)
+                .filter(function()
+                    {
+                    return this.name == key && ($(this).val() == value || $(this).is('[type=checkbox]'));
+                    })
+                .prop('checked', !!value);
+            */
+            var changed = { elemsChecked: null, elemsVal: null };
+            
+            changed.elemsChecked = $element.find(selectorElemsChecked)
+                .filter('[name="'+ key +'"]')
+                .prop('checked', false)
+                .filter(function() { return $(this).val() == value; })
+                .prop('checked', true);
+            changed.elemsVal = $element.find(selectorElemsVal)
+                .filter('[name="'+ key +'"]')
+                .val(value);
+            
+            if (!changed.elemsChecked.length && !changed.elemsVal.length)
+                {
+                if (typeof cfg.onUnknown == 'function')
+                    cfg.onUnknown.call($element, key, value, keyvals);
+                }
+            else
+                {
+                if (cfg.triggerChange && cfg.triggerChange.indexOf(key) > -1)
+                    {
+                    changed.elemsChecked && changed.elemsChecked.length ? changed.elemsChecked.change() : 0;
+                    changed.elemsVal && changed.elemsVal.length ? changed.elemsVal.change() : 0;
+                    }
+                }
+            }
+        }
+        
+    function decodeURIComponentPlus(s)
+        {
+        return decodeURIComponent(s ? (s+'').replace(/\+/g, '%20') : '');
+        }
+    
+    function toOneLevelObject(o, prefix)
+        {
+        if (typeof o != 'object')
+            return o;
+        
+        var res = {};
+        for (var k in o)
+            {
+            var key = prefix ? prefix + '['+k+']' : k;
+            if (typeof o[k] != 'object')
+                res[key] = o[k];
+            else
+                {
+                var o2 = toOneLevelObject(o[k], key);
+                for (var innerKey in o2)
+                    {
+                    res[innerKey] = o2[innerKey];
+                    }
+                }
+            }
+        
+        return res;
+        }
+    
+    })(jQuery);
